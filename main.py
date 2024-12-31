009fc5a34df27c2e8e323b13e999668f0ebdbe12
amarjit <amarjit.yadav@bitsflowtech.com>
2024-12-31 16:07:33 +0530

    added logic to save query into a file and then zip it into a tar file

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import os
+import time
+import datetime
+import tarfile
 
 # Get the current working directory
+
 CURRENT_PATH = os.getcwd()
 BKP_PATH = os.path.join(CURRENT_PATH,"bkp")
 # create bkp file if not exists
@@ -28,6 +32,10 @@ password =args.password
 db_name =args.db_name
 destination =args.destination
 
+ts = time.time()
+datetime_string = datetime.datetime.fromtimestamp(ts).strftime('%Y_%m_%d_%H_%M_%S')
+CURRENT_BKP_FOLDER = os.path.join(BKP_PATH,datetime_string)
+os.makedirs(CURRENT_BKP_FOLDER)
 
 # connect to db
 
@@ -66,6 +74,9 @@ def fetch_tables(cursor):
     result = cursor.execute("SHOW TABLES;")
 
     all_tables = cursor.fetchall()
+    
+    
+
 
     for table_name in all_tables:
 
@@ -86,24 +97,45 @@ def fetch_tables(cursor):
             values = ', '.join(f"'{str(value).replace('\'', '\\\'')}'" if value is not None else 'NULL' for value in row)
             insert_statements.append(f"INSERT INTO {table_name} ({', '.join(columns)}) VALUES ({values});")
 
-        print('insert_statements',insert_statements)
+        # print('insert_statements',insert_statements)
         
         table_desc_qry = f"SHOW CREATE TABLE {table_name}"
         cursor.execute(table_desc_qry)
         create_table_query = cursor.fetchall()
         
-        # write_to_file(table_name,table_data)
+        
+        
+        write_to_file(table_name,create_table_query,insert_statements)
 
 
-def write_to_file(table_name,table_data):
-    pass
+def write_to_file(table_name,create_query,insert_queries):
+    filename = f"{db_name}_{table_name}.txt"
+    current_file = os.path.join(CURRENT_BKP_FOLDER,filename)
+    
+    f = open(current_file, "w")
+    
+    write_context = f"""{str(create_query[0][1])}
 
-connect_to_db()
-# get data of each table and write to file
-# check for ways to write it to a file
+{"\n".join(insert_queries)}
+"""
+    
+    f.write(write_context)
+    f.close()
+    
+
+def make_tarfile():
+    output_filename = f"{db_name}_{datetime_string}.tar"
+    with tarfile.open(output_filename, "w:gz") as tar:
+        tar.add(CURRENT_BKP_FOLDER, arcname=os.path.basename(CURRENT_BKP_FOLDER))
 
 # use gzip to zip file into smaller size.
 
 # v2
 
 # use pandas to read csv file and write to db
+
+
+if __name__ == "__main__":
+    connect_to_db()
+    # fetch_tables()
+    make_tarfile()
